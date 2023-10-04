84bd145b1f193e8acfbfc8e89d1d6a33abfbb3a2
ChongZhiJie <ChongZhiJie021604@gmail.com>
2023-10-04 15:29:48 +0800

    Change

--- Command Promt/oh-my-posh.lua
+++ Command Promt/oh-my-posh.lua
@@ -1,1 +1,1 @@
-load(io.popen('oh-my-posh init cmd --config https://raw.githubusercontent.com/ChongZhiJie0216/Env-Terminal/main/blueish.omp.json'):read("*a"))()
+load(io.popen('oh-my-posh init cmd --config https://raw.githubusercontent.com/ChongZhiJie0216/Env-Terminal/main/blueish%2BEDM115.omp.json'):read("*a"))()
--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,2 @@
 Import-Module -Name Terminal-Icons
-oh-my-posh prompt init pwsh --config "https://raw.githubusercontent.com/ChongZhiJie0216/Env-Terminal/main/blueish.omp.json" | Invoke-Expression
+oh-my-posh prompt init pwsh --config "https://raw.githubusercontent.com/ChongZhiJie0216/Env-Terminal/main/blueish%2BEDM115.omp.json" | Invoke-Expression
